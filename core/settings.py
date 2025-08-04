805ab72d18db3f88bdd800f851f119d88df47e60
evdias2005 <evdias2005@gmail.com>
2025-08-04 17:15:05 -0300

    Nova tentativa de correção dos arquivos estáticos - 17:15

--- core/settings.py
+++ core/settings.py
@@ -121,15 +121,12 @@ USE_THOUSAND_SEPARATOR = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
-
-# Diretório onde o Django coletará os arquivos estáticos (usado em produção)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
-# # Diretórios adicionais para buscar arquivos estáticos (usado no desenvolvimento)
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'homepage/static')
-# ]
+
 
 MEDIA_URL = '/media/'
 
