8014b3a3c4cef622cff1b850ee678cec9ea6905b
evdias2005 <evdias2005@gmail.com>
2025-08-04 14:54:43 -0300

    Mudança em todo ambiente para a versão python3.13 juntamente com Django 5.2

--- core/settings.py
+++ core/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
     'widget_tweaks',                            # uses 'django-widget-tweaks' app
     'crispy_forms',                             # uses 'django-crispy-forms' app
+    'crispy_bootstrap4',
     'login_required',                           # uses 'django-login-required-middleware' app
 
     'homepage.apps.HomepageConfig',
@@ -138,6 +139,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'documentos/')
 #
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'                     # bootstrap template crispy-form uses
 
 LOGIN_REDIRECT_URL = 'home'                             # sets the login redirect to the 'home' page after login
